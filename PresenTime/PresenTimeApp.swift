a7a3f66ee71931dce4410c35c1280236fe5de4a1
Hasan Berat Gürbüz <hasangurbuz.business@hotmail.com>
2025-01-10 20:55:42 +0300

    Initial Commit

---
+++ PresenTime/PresenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresenTimeApp.swift
+//  PresenTime
+//
+//  Created by Hasan Berat Gürbüz on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct PresenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
